7442ac194b5f242ec62e07222ab1953bdef00186
Laurindo Missengo <laurindosunset@gmail.com>
2025-06-20 14:18:26 +0100

    Add image to hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import BackGround from "./components/BackGround";
 import NavBar from "./components/NavBar";
+import HeroPage from "./pages/HeroPage";
 
 function App() {
   return (
     <BackGround>
       <NavBar />
+      <HeroPage />
     </BackGround>
   );
 }
--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -1,12 +1,13 @@
 import BackGround from "../components/BackGround";
 import NavBar from "../components/NavBar";
+import profileImage from "../assets/images/profile.png";
 
 function HeroPage() {
   return (
-    <BackGround>
-      <NavBar />
+    <div>
       <h1>Hero Page</h1>
-    </BackGround>
+      <img src={profileImage} alt="Mr. Light" />
+    </div>
   );
 }
 export default HeroPage;
